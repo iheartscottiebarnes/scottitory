bf32584b937f69f732cf5e7de989310c2de7d957
phillip t <raexc47@gmail.com>
2026-07-20 14:25:50 -0700

    Add files via upload

---
+++ gaem2.py
@@ -0,0 +1,13 @@
+stats = {
+    'ppg': [18.1, 26.1, 23.9, 19.5, 28.8, 17.1, 22.5, 28.3, 28.7, 26, 26, 18.6, 21.5, 20.9, 27.9, 
+            27.9, 26.6, 24.2, 33.5, 31.1, 27.7, 25.4, 21.7, 20.4, 24, 20.1, 25, 27.6]
+}
+allstarsdict = {
+    "Names": ['Scottie Barnes', 'Devin Booker', 'Cade Cunningham', 'Jalen Duren', 'Anthony Edwards', 'Chet Holmgren',
+    'Jalen Johnson', 'Tyrese Maxey', 'Jaylen Brown', 'Jalen Brunson', 'Kevin Durant', 'DeAaron Fox',
+    'Brandon Ingram', 'LeBron James', 'Kawhi Leonard', 'Donovan Mitchell', 'Stephen Curry', 'Deni Avdija',
+    'Luka Doncic', 'Shai Gilgeous-Alexander', 'Nikola Jokic', 'Jamal Murray', 'Norman Powell', 
+    'Alperen Sengun', 'Pascal Siakam', 'Karl-Anthony Towns', 'Victor Wembanyama', 'Giannis Antetokounmpo']
+    
+}
+print(*stats['ppg'], sep=', ')
